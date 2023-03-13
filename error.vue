50495a3a8019a01326f12ff7bd135f9a4ab3c1f3
Cyrus Yip <cyruscmyip1@gmail.com>
2023-03-12 17:03:51 -0700

    show better error messages

--- error.vue
+++ error.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <h1>{{ error.name }}</h1>
-    <h2>{{ error.message }}</h2>
+  <div
+    class="min-h-screen flex flex-col items-center justify-center text-sans2 gap-4"
+  >
+    <h1 class="text-3xl">Error {{ error.statusCode }}</h1>
+    <h2 class="text-2xl">{{ error.message }}</h2>
+    <ButtonLink to="/">Go Home</ButtonLink>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  error: Error;
+import { NuxtError } from "#app";
+
+const props = defineProps<{
+  error: NuxtError;
 }>();
+onMounted(() => {
+  console.error(props.error);
+});
 </script>
